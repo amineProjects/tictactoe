6e07b7d059c9af6a157595f59ec94bec53d2c3e4
amine benahmed <amineforfriends@gmail.com>
2020-02-07 02:08:41 +0100

    feat: add player input

--- src/index.js
+++ src/index.js
@@ -20,7 +20,9 @@ let board = [
     ["", "", ""]
   ],
   human = "X",
-  computer = "O";
+  computer = "O",
+  notAvailable = [],
+  currentPlayer = human;
 
 const game = new Phaser.Game(config);
 
@@ -29,11 +31,46 @@ function preload() {}
 function create() {
   let cellWidth = width / board.length;
   let cellHeight = height / board.length;
-  console.log(cellHeight, cellWidth, width, height);
   //create board
-  this.add
+  let grid = this.add
     .grid(0, 0, width, height, cellWidth, cellHeight, 0x00b9f2)
     .setAltFillStyle(0x016fce)
     .setOutlineStyle()
     .setOrigin(0, 0);
+  grid.setInteractive();
+  grid.on("pointerup", humanPlay(cellWidth, cellHeight, this), this);
+  console.log(grid);
 }
+
+const humanPlay = (cellWidth, cellHeight, secne) => (pointer, x, y) => {
+  if (currentPlayer === computer) return;
+
+  let cellPosition = getCellPosition(x, y, cellWidth, cellHeight);
+  console.log(x, y, cellPosition, notAvailable);
+  if (isNotAvailable(notAvailable, cellPosition)) return;
+  notAvailable = setNotAvailable(notAvailable, cellPosition);
+  console.log(notAvailable);
+  secne.add.text(
+    (cellWidth / 2) * cellPosition[0],
+    (cellHeight / 2) * cellPosition[1],
+    human
+  );
+  currentPlayer = computer;
+};
+
+const getCellPosition = (x, y, cellWidth, cellHeight) => {
+  return [Math.ceil(x / cellWidth), Math.ceil(y / cellHeight)];
+};
+const setNotAvailable = (notAvailable, cellPosition) => {
+  return [...notAvailable, cellPosition];
+};
+const isNotAvailable = (notAvailable, cellPosition) => {
+  if (notAvailable.length === 0) return false;
+  return !notAvailable.some(cp => {
+    console.log(
+      "inside isAvailable",
+      cp[0] === cellPosition[0] && cp[1] === cellPosition[1]
+    );
+    return cp[0] === cellPosition[0] && cp[1] === cellPosition[1];
+  });
+};
