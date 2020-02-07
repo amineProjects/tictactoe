1a5ec008a19b99b61c663cd743c7b145eacc19d3
amine benahmed <amineforfriends@gmail.com>
2020-02-07 09:55:14 +0100

    feat(ai):create ai player

--- src/index.js
+++ src/index.js
@@ -14,11 +14,12 @@ const config = {
   }
 };
 
-let board = [
-    ["", "", ""],
-    ["", "", ""],
-    ["", "", ""]
-  ],
+// let board = [
+//     ["", "", ""],
+//     ["", "", ""],
+//     ["", "", ""]
+//   ],
+let board = 3,
   human = "X",
   computer = "O",
   notAvailable = [],
@@ -29,8 +30,8 @@ const game = new Phaser.Game(config);
 function preload() {}
 
 function create() {
-  let cellWidth = width / board.length;
-  let cellHeight = height / board.length;
+  let cellWidth = width / board;
+  let cellHeight = height / board;
   //create board
   let grid = this.add
     .grid(0, 0, width, height, cellWidth, cellHeight, 0x00b9f2)
@@ -42,23 +43,46 @@ function create() {
   console.log(grid);
 }
 
-const humanPlay = (cellWidth, cellHeight, secne) => (pointer, x, y) => {
+const humanPlay = (cellWidth, cellHeight, scene) => (pointer, x, y) => {
   if (currentPlayer === computer) return;
 
   let cellPosition = getCellPosition(x, y, cellWidth, cellHeight);
   console.log(x, y, cellPosition, notAvailable);
   if (!isNotAvailable(notAvailable, cellPosition)) return;
   notAvailable = setNotAvailable(notAvailable, cellPosition);
+  console.log(notAvailable, scene);
+  playMove(scene, cellWidth, cellHeight, human, cellPosition);
+  currentPlayer = computer;
+  computerPlay(notAvailable, scene, cellWidth, cellHeight);
+};
+
+const computerPlay = (notAvailableMove, scene, cellWidth, cellHeight) => {
+  if (notAvailable.length === 9 || currentPlayer === human) return;
+  let cellPosition = nextMove(board);
+
+  if (!isNotAvailable(notAvailableMove, cellPosition)) {
+    return computerPlay(notAvailableMove, scene, cellWidth, cellHeight);
+  }
+  notAvailable = setNotAvailable(notAvailableMove, cellPosition);
   console.log(notAvailable);
-  secne.add
+  playMove(scene, cellWidth, cellHeight, computer, cellPosition);
+  currentPlayer = human;
+};
+
+const nextMove = board => {
+  return [Phaser.Math.RND.between(1, board), Phaser.Math.RND.between(1, board)];
+};
+
+const playMove = (scene, cellWidth, cellHeight, player, cellPosition) => {
+  console.log(scene);
+  scene.add
     .text(
       cellWidth * cellPosition[0] - cellWidth / 2,
       cellHeight * cellPosition[1] - cellHeight / 2,
-      human,
+      player,
       { fontSize: "166px" }
     )
     .setOrigin(0.5);
-  currentPlayer = computer;
 };
 
 const getCellPosition = (x, y, cellWidth, cellHeight) => {
